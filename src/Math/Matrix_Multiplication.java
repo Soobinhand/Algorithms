63b791e5004374a7660c8bdd5b093191df8f1560
Soobinhand <chelsea9528@naver.com>
2021-12-28 21:31:11 +0900

    행렬 곱 알고리즘

---
+++ src/Math/Matrix_Multiplication.java
@@ -0,0 +1,29 @@
+package Math;
+
+import java.util.Arrays;
+
+/**
+ 두 행렬의 곱을 구하는 솔루션.
+ */
+
+public class Matrix_Multiplication {
+
+    public static int[][] matrix_multiplication(int[][] a, int[][] b){
+        int[][] answer = new int[a.length][b[0].length];
+        for (int i =0; i<a.length;i++){
+            for(int j=0;j<b[0].length;j++){
+                for (int k=0;k<a[0].length;k++){
+                    answer[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+
+    // 테스트 클라이언트
+    public static void main(String[] args) {
+        int[][] a = new int[][]{{1,2},{3,4}};
+        int[][] b = new int[][]{{1,2,3},{3,4,4}};
+        System.out.println(Arrays.deepToString(matrix_multiplication(a, b)));
+    }
+}
